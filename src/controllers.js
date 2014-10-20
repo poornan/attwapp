0dca77b36521081fa06f700e594c7f8181407a9d
E Ananthaneshan <poornan@gmail.com>
2014-10-21 00:10:06 +0530

    test add library

--- src/controllers.js
+++ src/controllers.js
@@ -1,6 +1,6 @@
 (function(){
 var addLibrary = angular.module('addLibrary', []);
-addLibrary.controller('AddController', ['$http', function($http){
+addLibrary.controller('AddController', ['$http', transformRequestAsFormPost, function($http, transformRequestAsFormPost){
   this.formData = {};
   this.processForm = function() {
                   console.log(this.formData);
@@ -8,7 +8,8 @@ addLibrary.controller('AddController', ['$http', function($http){
                           method  : 'POST',
                           url     : 'https://appserver.dev.cloud.wso2.com/t/ananthanesh4519/webapps/attws-default-SNAPSHOT/services/library/libraryService/library',
                           data    : this.formData,  // pass in data as strings
-                          headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+                          transformRequest: transformRequestAsFormPost
+                          //headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
                       })
                           .success(function(data) {
                               console.log(data);
@@ -26,6 +27,81 @@ addLibrary.controller('AddController', ['$http', function($http){
   			};
 
 }]);
+
+addLibrary.factory(
+            "transformRequestAsFormPost",
+            function() {
+
+                // I prepare the request data for the form post.
+                function transformRequest( data, getHeaders ) {
+
+                    var headers = getHeaders();
+
+                    headers[ "Content-type" ] = "application/x-www-form-urlencoded; charset=utf-8";
+
+                    return( serializeData( data ) );
+
+                }
+
+
+                // Return the factory value.
+                return( transformRequest );
+
+
+                // ---
+                // PRVIATE METHODS.
+                // ---
+
+
+                // I serialize the given Object into a key-value pair string. This
+                // method expects an object and will default to the toString() method.
+                // --
+                // NOTE: This is an atered version of the jQuery.param() method which
+                // will serialize a data collection for Form posting.
+                // --
+                // https://github.com/jquery/jquery/blob/master/src/serialize.js#L45
+                function serializeData( data ) {
+
+                    // If this is not an object, defer to native stringification.
+                    if ( ! angular.isObject( data ) ) {
+
+                        return( ( data == null ) ? "" : data.toString() );
+
+                    }
+
+                    var buffer = [];
+
+                    // Serialize each key in the object.
+                    for ( var name in data ) {
+
+                        if ( ! data.hasOwnProperty( name ) ) {
+
+                            continue;
+
+                        }
+
+                        var value = data[ name ];
+
+                        buffer.push(
+                            encodeURIComponent( name ) +
+                            "=" +
+                            encodeURIComponent( ( value == null ) ? "" : value )
+                        );
+
+                    }
+
+                    // Serialize the buffer and clean it up for transportation.
+                    var source = buffer
+                        .join( "&" )
+                        .replace( /%20/g, "+" )
+                    ;
+
+                    return( source );
+
+                }
+
+            }
+        );
 
 var app = angular.module('ATT', []);
 /*app.config(function ($httpProvider) {
